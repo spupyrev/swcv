7fb51030bcbf07f82ac944ede769ec09e699f7de
JixianLi <jixianli@email.arizona.edu>
2014-03-06 15:32:44 -0700

    fixed style, logger
    I only fix this logger for now. I believe we should create a logger
    class so every object should be able to use it.

--- webapp/src/edu/webapp/server/readers/TwitterReader.java
+++ webapp/src/edu/webapp/server/readers/TwitterReader.java
@@ -1,67 +1,90 @@
 package edu.webapp.server.readers;
-import java.util.List;
 
+import java.util.List;
+import java.util.logging.Logger;
+
+import edu.webapp.server.WordCloudServiceImpl;
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
-public class TwitterReader implements IDocumentReader{
+
+public class TwitterReader implements IDocumentReader
+{
 	
+	private static final Logger log = Logger.getLogger(TwitterReader.class.getName());
 	private String text;
-	
+
 	private Twitter twitter;
-	
-	@Override
-	public boolean isConnected(String input) {
-		text = new String();
-		return searchTweets(input);
-	}
+
+	private String QueryLang = "en";
+	private String resultType = Query.MIXED;
+	private int QueryCount = 100;
+	private String unwantedPattern = "\\s*http[s]?://\\S+\\s*";
 
 	@Override
-	public String getText(String input) {
-		return text;
-	}
-
-	private boolean searchTweets(String input){
-		auth();
-		if (input.startsWith("twitter:")){
-			String[] tags = input.split("\\s+");
-			String q = new String();
-			for (int i = 1; i<tags.length; i++){
-				q += tags[i]+" ";
-			}
-			try {
-	            Query query = new Query(q);
-	            query.setCount(100);
-	            query.setLang("en");
-	            query.setResultType(Query.MIXED);
-	            QueryResult result;
-	            do {
-	                result = twitter.search(query);
-	                List<Status> tweets = result.getTweets();
-	                for (Status tweet : tweets) {
-	                    text += removeLinks(tweet.getText());
-	                }
-	            } while ((query = result.nextQuery()) != null);
-	            return true;
-	        } catch (TwitterException te) {
-	            te.printStackTrace();
-	            System.out.println("Failed to search tweets: " + te.getMessage());
-	            return false;
-	        }
+	public boolean isConnected(String input)
+	{
+		if (input.startsWith("twitter: "))
+		{
+			return searchTweets(input);
 		}
 		return false;
 	}
 
-	private String removeLinks(String tweet) {
-		return tweet.replaceAll("\\s+http://\\S+\\s*", "");
+	@Override
+	public String getText(String input)
+	{
+		return text;
 	}
 
-	private void auth() {
+	private boolean searchTweets(String input)
+	{
+
+		auth();
+		String[] tags = input.split("\\s+");
+		String q = new String();
+		for (int i = 1; i < tags.length; i++)
+		{
+			q += tags[i] + " ";
+		}
+		try
+		{
+			Query query = new Query(q);
+			query.setCount(QueryCount);
+			query.setLang(QueryLang);
+			query.setResultType(resultType);
+			QueryResult result;
+			text = new String();
+			do
+			{
+				result = twitter.search(query);
+				List<Status> tweets = result.getTweets();
+				for (Status tweet : tweets)
+				{
+					text += removeLinks(tweet.getText());
+				}
+			} while ((query = result.nextQuery()) != null);
+			return true;
+		}
+		catch (TwitterException te)
+		{
+			log.info(te.getMessage());
+			return false;
+		}
+	}
+
+	private String removeLinks(String tweet)
+	{
+		return tweet.replaceAll(unwantedPattern, "");
+	}
+
+	private void auth()
+	{
 		ConfigurationBuilder cb = new ConfigurationBuilder();
-		cb.setDebugEnabled(false)
-		  .setOAuthConsumerKey("")
-		  .setOAuthConsumerSecret("")
-		  .setOAuthAccessToken("")
-		  .setOAuthAccessTokenSecret("");
+		cb.setDebugEnabled(false);
+		cb.setOAuthConsumerKey("");
+		cb.setOAuthConsumerSecret("");
+		cb.setOAuthAccessToken("");
+		cb.setOAuthAccessTokenSecret("");
 		TwitterFactory tf = new TwitterFactory(cb.build());
 		twitter = tf.getInstance();
 	}
