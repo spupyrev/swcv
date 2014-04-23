510069de51b465c41e6d9aa194a70b03db6ed2bb
JixianLi <jixianli@email.arizona.edu>
2014-04-23 13:23:01 -0700

    reupload WorCloudServiceImpl

---
+++ webapp/src/edu/webapp/server/WordCloudServiceImpl.java
@@ -0,0 +1,39 @@
+package edu.webapp.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+import edu.webapp.client.WordCloudService;
+import edu.webapp.server.utils.RandomTwitterTrendExtractor;
+import edu.webapp.server.utils.RandomWikiUrlExtractor;
+import edu.webapp.server.utils.RandomYoutubeUrlExtractor;
+import edu.webapp.shared.WCSetting;
+import edu.webapp.shared.WordCloud;
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class WordCloudServiceImpl extends RemoteServiceServlet implements WordCloudService
+{
+
+	public WordCloud buildWordCloud(String input, WCSetting setting) throws IllegalArgumentException
+	{
+		String ip = getThreadLocalRequest().getRemoteAddr();
+		return WordCloudGenerator.buildWordCloud(input, setting, ip);
+	}
+
+	public String getRandomWikiUrl()
+	{
+		return RandomWikiUrlExtractor.getRandomWikiPage();
+	}
+
+	public String getRandomTwitterUrl()
+	{
+		return RandomTwitterTrendExtractor.getRandomTrend();
+	}
+
+	public String getRandomYoutubeUrl()
+	{
+		return RandomYoutubeUrlExtractor.getRandomUrl();
+	}
+}
