d932fdd762fd1ed16afb9e22313f12a52d677121
Sergey Pupyrev <Sergey Pupyrev@SergeyDell.(none)>
2014-10-04 13:01:57 -0700

    twitter

--- webapp/src/edu/webapp/server/readers/TwitterCredentials.java
+++ webapp/src/edu/webapp/server/readers/TwitterCredentials.java
@@ -2,8 +2,8 @@ package edu.webapp.server.readers;
 
 public class TwitterCredentials
 {
-    public static final String CONSUMER_KEY = "OxGHWmfAzhCtRjq4l161Gg";
-    public static final String CONSUMER_SECRET = "uMDzxLd4L8dJ08MTNs6RP3SCEgqjgSoQUqttCAO31I";
-    public static final String ACCESS_TOKEN = "2352327474-soCM0anV3VW1unwUxlem0ypQ6Ysdu0RprLlLaPz";
-    public static final String ACCESS_TOKEN_SECRET = "ZIGCCIsyWq6RLg8qBZzRrEmoqmANc6UaGeYJ45QYBIRc4";
+    public static final String CONSUMER_KEY = "";
+    public static final String CONSUMER_SECRET = "";
+    public static final String ACCESS_TOKEN = "";
+    public static final String ACCESS_TOKEN_SECRET = "";
 }
